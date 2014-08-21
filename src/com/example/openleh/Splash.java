0413e36f5d390f474d6929b328f47ac9b570c121
Adam <Adam@Adams-MacBook-Air.local>
2014-08-21 23:20:58 +0800

    1st Version
    CZ2006 Software Project

---
+++ src/com/example/openleh/Splash.java
@@ -0,0 +1,21 @@
+package com.example.openleh;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class Splash extends Activity {
+
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.splash);
+	    }
+	
+	Handler x = new Handler(); 
+	
+	
+}
+
