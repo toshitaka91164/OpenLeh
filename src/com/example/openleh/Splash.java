bde18107de024a848de86122d883fbfa13230ff0
Adam <Adam@Adams-MacBook-Air.local>
2014-09-02 20:47:42 +0800

    Splash Screen Activity
    Added Splash Screen Activity to the app

--- src/com/example/openleh/Splash.java
+++ src/com/example/openleh/Splash.java
@@ -1,21 +1,38 @@
 package com.example.openleh;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-
-
 
 public class Splash extends Activity {
+	private long splashDelay = 5000; //5 seconds
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        TimerTask task = new TimerTask()
+        {
 
-	 @Override
-	    protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.splash);
-	    }
-	
-	Handler x = new Handler(); 
-	
-	
+			@Override
+			public void run() {
+				finish();
+				Intent mainIntent = new Intent().setClass(Splash.this, MainActivity.class);
+				startActivity(mainIntent);
+			}
+        	
+        };
+        
+        Timer timer = new Timer();
+        timer.schedule(task, splashDelay);
+    }
 }
-
